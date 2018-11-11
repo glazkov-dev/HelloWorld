2c8b4475073718cdef8eaef712eec947874345e0
glazkov-dev <34450575+glazkov-dev@users.noreply.github.com>
2018-11-11 17:19:36 +0300

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Thread.Sleep(1000);
+            Console.WriteLine("Wake up, Neo...");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("The Matrix has you...");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Thread.Sleep(2000);
+            Console.ReadKey();
+        }
+    }
+}
